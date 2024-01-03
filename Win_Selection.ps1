c60e0b79ceb106a44c7ed5426f2309496e8aebd1
EMYScripts <155473650+EMYScripts@users.noreply.github.com>
2024-01-03 18:14:39 -0500

    Update Win_Selection.ps1

--- Win_Selection.ps1
+++ Win_Selection.ps1
@@ -2,8 +2,8 @@
 Start-Sleep -Seconds 5
 
 #Latitude 3500 Series
-Write-Host  -ForegroundColor Green "Latitude - BIOS Setting"
-cmd.exe /c D:\CCTK\cctk.exe --EmbSataRaid=Ahci
+#Write-Host  -ForegroundColor Green "Latitude - BIOS Setting"
+#cmd.exe /c D:\CCTK\cctk.exe --EmbSataRaid=Ahci
 
 #Optiplex 3000 Series
 #Write-Host  -ForegroundColor Green "Optiplex - BIOS Setting"
