06bc937764af389b65408fa5d185a79c3fef7c94
adamsyaj <155473650+adamsyaj@users.noreply.github.com>
2024-01-03 08:31:06 -0500

    Update Win_Selection.ps1

--- Win_Selection.ps1
+++ Win_Selection.ps1
@@ -6,6 +6,6 @@ Write-Host  -ForegroundColor Green "Start OSDCloud"
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -OSLanguage en-us -OSLicense Volume -ZTI
 
 #Restart from WinPE
-Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
-wpeutil reboot
+#Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+#Start-Sleep -Seconds 20
+#wpeutil reboot
