5cb72d2792d4866012edd163654af17ee96a5004
EMYScripts <155473650+EMYScripts@users.noreply.github.com>
2024-01-03 17:21:18 -0500

    Update Win_Selection.ps1

--- Win_Selection.ps1
+++ Win_Selection.ps1
@@ -2,9 +2,11 @@
 Start-Sleep -Seconds 5
 
 #Latitude 3500 Series
+Write-Host  -ForegroundColor Green "Latitude - BIOS Setting"
 cmd.exe /c D:\CCTK\cctk.exe --EmbSataRaid=Ahci
 
 #Optiplex 3000 Series
+Write-Host  -ForegroundColor Green "Optiplex - BIOS Setting"
 #cmd.exe /c D:\CCTK\cctk.exe --AcPwrRcvry=on --WakeOnLan=LanOnly --AutoOn=Everyday --AutoOnHr=00 --AutoOnMn=00 --DeepSleepCtrl=Disabled --BlockSleep=Enabled --EmbSataRaid=Ahci
 
 #Start OSDCloud ZTI the RIGHT way
