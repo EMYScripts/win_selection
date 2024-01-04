0917cd65f3a76090332aef2479be2b2108a0c2c9
EMYScripts <155473650+EMYScripts@users.noreply.github.com>
2024-01-04 17:37:37 -0500

    Update Win_Selection.ps1

--- Win_Selection.ps1
+++ Win_Selection.ps1
@@ -14,8 +14,8 @@ Write-Host  -ForegroundColor Green "Start OSDCloud"
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -OSLanguage en-us -OSLicense Volume -ZTI
 
 #Download Unattend.xml 
-#Write-Host  -ForegroundColor Green "Downloading XML"
-#Invoke-WebRequest -Uri "https://raw.githubusercontent.com/EMYScripts/win_selection/main/unattend.xml" -OutFile "C:\windows\panther\unattend.xml"
+Write-Host  -ForegroundColor Green "Downloading XML"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/EMYScripts/win_selection/main/unattend.xml" -OutFile "C:\windows\panther\unattend.xml"
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
