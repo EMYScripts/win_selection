7090f68dd8a0bd95fe1326e6036c1c4441afbe49
EMYScripts <155473650+EMYScripts@users.noreply.github.com>
2024-01-04 18:54:52 -0500

    Update Additional_Settings.ps1

--- Additional_Settings.ps1
+++ Additional_Settings.ps1
@@ -1,10 +1,15 @@
-﻿#Remove News Feeds & Widget
+#Remove News Feeds & Widget
 reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" /v EnableFeeds /t REG_DWORD /d 0 /f
 reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Dsh" /v AllowNewsAndInterests /t REG_DWORD /d 0 /f
 ECHO Yes | Winget uninstall "Windows Web Experience pack"
 
+
+reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v EnableDynamicContentInWSB /t REG_DWORD /d 0 /f
+
 #Remove Windows Store
 reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\WindowsStore" /v RemoveWindowsStore /t REG_DWORD /d 1 /f
+
+Get-appxpackage -allusers *Microsoft.WindowsStore* | Remove-AppxPackage -allusers
 
 #OSDCloud Cleanup	
 Remove-Item -Path C:\Drivers, C:\OSDCloud -Recurse -Force
